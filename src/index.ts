45dd54d2bbfbcce794dfede97234f8ce6519966c
Dmitry Loshakov <loshakovdmitry@ya.ru>
2025-03-10 11:30:27 +0300

    architecture_check

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
 import './scss/styles.scss';
+import { Api } from './components/base/api';
+
+const api = new Api('https://larek-api.nomoreparties.co/api/weblarek/');
+api.get('product/').then((result) => console.log(result));
+api.get('product/c101ab44-ed99-4a54-990d-47aa2bb4e7d9').then((result) => console.log(result));
+
+
